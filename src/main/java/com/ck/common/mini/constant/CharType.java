ee6893d618a0f2e0884e9294fce2e1db57b29e4a
caikun1 <caikun@baidu-mgame.com>
2020-11-04 17:29:28 +0800

    正式推出全匹配功能！

---
+++ src/main/java/com/ck/common/mini/constant/CharType.java
@@ -0,0 +1,15 @@
+package com.ck.common.mini.constant;
+
+/**
+ * @Author caikun
+ * @Description 字符类型
+ * @Date 下午5:18 20-11-4
+ **/
+public enum CharType {
+
+    BLANK,
+    DIGIT,
+    ALP,
+    BIGCHAR,
+    OTHER;
+}
